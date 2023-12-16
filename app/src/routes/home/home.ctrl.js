22c5ebd5d8c1ab28ff559c1ebb13bfb50a45811c
sopand <pows1031@naver.com>
2023-12-16 21:14:08 +0900

    로그인 Model 생성

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,41 +1,37 @@
 "use strict";
 
+const UserStorage =require("../../models/UserStorage");
 
-const users={
-    id:["pows1011","pows1021","pows1031"],
-    psword:["1234","2345","3456"],
-}
 
-const output={
-    home: (req, res) => {
-        res.render("home/index");
-    },
-    login :  (req, res) => {
-        res.render("home/login");
-    },
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const process={
-    login:(req,res)=>{
-        const id=req.body.id,
-            psword=req.body.psword;
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                return res.json({
-                    success:true,
-                });
-            };
-        }
-        return res.json({
-            success:false,
-            msg:"로그인 실패요",
-        });
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+    const users=UserStorage.getUsers("id","psword");
+    const response = {};
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        response.success = true;
+        return res.json(response);
+      }
     }
+    response.success = false;
+    response.msg = "로그인에 실패함";
+    return res.json(response);
+  },
+};
 
-}
-
-module.exports={
+module.exports = {
   output,
   process,
 };
