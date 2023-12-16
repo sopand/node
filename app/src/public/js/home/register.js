22e1de9309ce500efeccee4f15d23e26f3e3a55d
sopand <pows1031@naver.com>
2023-12-16 22:24:43 +0900

    회원가입 화면

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,21 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
+    confirmPsword = document.querySelector("#confirm-psword"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    registerBtn = document.querySelector("#button");
 
-    loginBtn.addEventListener("click",login);
 
-    function login(){
+    registerBtn.addEventListener("click",register);
+    function register(){
         const req= {
             id: id.value,
+            name : name.value,
             psword : psword.value,
+            confirmPsword :confirmPsword.value,
         };
-        
-        fetch("/login",{
+        fetch("/register",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json",
@@ -23,12 +26,14 @@ const id = document.querySelector("#id"),
         // .then(console.log);
         .then((res)=>{
             if(res.success){
-                location.href="/";
+                location.href="/login";
             }else{
                 alert(res.msg);
             }
         })
         .catch((err)=>{
-            console.error(new Error("로그인 중 에러 발생"));
+            console.error(new Error("회원가입 중 에러 발생"));
         })
     }
+
+    
