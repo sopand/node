cb3ef741520a41ab81db956c54cc74d2698540ea
sopand <pows1031@naver.com>
2023-12-16 22:40:26 +0900

    js 회원가입 유효성검사

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -21,25 +21,12 @@ const process = {
     const user=new User(req.body);
     const response=user.login();
     return res.json(response);
-    // return res.json(response);
-
-
-
-    // const id = req.body.id,
-    //   psword = req.body.psword;
-    // const users=UserStorage.getUsers("id","psword");
-    // const response = {};
-    // if (users.id.includes(id)) {
-    //   const idx = users.id.indexOf(id);
-    //   if (users.psword[idx] === psword) {
-    //     response.success = true;
-    //     return res.json(response);
-    //   }
-    // }
-    // response.success = false;
-    // response.msg = "로그인에 실패함";
-    // return res.json(response);
   },
+  register:(req,res)=>{
+    const user=new User(req.body);
+    const response=user.register();
+    return res.json(response);
+  }
 };
 
 module.exports = {
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -10,4 +10,5 @@ router.get('/login',ctrl.output.login);
 router.get('/register',ctrl.output.register);
 
 router.post("/login",ctrl.process.login);
+router.post("/register",ctrl.process.register);
 module.exports=router; // 외부로 내보내기위한 명령어
