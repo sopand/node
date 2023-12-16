79eece9d69f347c24814f4cf2fb50a2321396cfd
sopand <pows1031@naver.com>
2023-12-16 20:37:48 +0900

    login 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -21,5 +21,15 @@ const id = document.querySelector("#id"),
             body:JSON.stringify(req),
         }).then((res)=>res.json())
         // .then((res)=>console.log(res)); 2개가 같은 뜻임
-        .then(console.log);
+        // .then(console.log);
+        .then((res)=>{
+            if(res.success){
+                location.href="/";
+            }else{
+                alert(res.msg);
+            }
+        })
+        .catch((err)=>{
+            console.error(new Error("로그인 중 에러 발생"));
+        })
     }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,5 +7,5 @@ const ctrl=require("./home.ctrl");
 
 router.get('/',ctrl.output.home);
 router.get('/login',ctrl.output.login);
-router.post("/login",ctrl.process.login)
+router.post("/login",ctrl.process.login);
 module.exports=router; // 외부로 내보내기위한 명령어
