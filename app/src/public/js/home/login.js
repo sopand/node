2336311f1d518e270fca7c8ab18217d4889cdfda
sopand <pows1031@naver.com>
2023-12-16 19:57:46 +0900

    jsFile

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
 
-console.log("hello");
+    loginBtn.addEventListener("click",login);
+
+    function login(){
+        const req= {
+            id: id.value,
+            psword : psword.value,
+        };
+        console.log(req);
+    }
+
+
