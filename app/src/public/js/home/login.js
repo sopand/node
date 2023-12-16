3a7d0ddc95de59af12b9fe4a479e72495c46b848
sopand <pows1031@naver.com>
2023-12-16 20:05:43 +0900

    api만들기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,7 +11,14 @@ const id = document.querySelector("#id"),
             id: id.value,
             psword : psword.value,
         };
-        console.log(req);
+
+
+        fetch("/login",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify(req),
+        });
+
     }
-
-
