d53aacc269b5725c9f7109db4a88928e6051b7be
sopand <pows1031@naver.com>
2023-12-18 14:17:20 +0900

    fs모듈을 이용한 회원가입 기능완료

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,8 +8,8 @@ class User {
     this.body = body;
   }
   async login() {
-    const client=this.body;
-    const { id,psword } =await UserStorage.getUserInfo(client.id);
+    const client = this.body;
+    const { id, psword } = await UserStorage.getUserInfo(client.id);
     if (id) {
       if (id === client.id && psword === client.psword) {
         return { success: true };
@@ -19,10 +19,14 @@ class User {
     return { success: false, msg: "존재하지 않는 아이디입니다." };
   }
 
-  register(){
-    const client=this.body;
-    const response=UserStorage.save(client);
-    return response;
+  async register() {
+    const client = this.body;
+    try {
+      const response = await UserStorage.save(client);
+      return response;
+    } catch (err) {
+      return { success : false , msg:err};
+    }
   }
 }
 module.exports = User;
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,29 +3,20 @@
 const fs = require("fs").promises;
 
 class UserStorage {
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
+  }
 
-    static #getUserInfo(data,id) {
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) => {
-          newUser[info] = users[info][idx];
-          return newUser;
-        }, {});
-        return userInfo;
-      }
-
-
-
-
-
-
-
-
-  // ...변수명으로 파라미터를 받아오면 해당 파라미터에 갯수 제한을 두는게아닌 배열로 받아와 여러 파라미터를 받을 수 있음
-  static getUsers(...fields) {
-    // 자바의 Getter원리인듯 함 바로 가져오는게 아닌 Getter를 거쳐서 데이터를 가져오는 것
-    // const users = this.#users;
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) return users;
     const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
@@ -36,20 +27,38 @@ class UserStorage {
     return newUsers;
   }
 
-  static getUserInfo(id) {
+  // ...변수명으로 파라미터를 받아오면 해당 파라미터에 갯수 제한을 두는게아닌 배열로 받아와 여러 파라미터를 받을 수 있음
+  static getUsers(isAll, ...fields) {
+    // 자바의 Getter원리인듯 함 바로 가져오는게 아닌 Getter를 거쳐서 데이터를 가져오는 것
+    // const users = this.#users;
+
     return fs
       .readFile("./src/databases/users.json")
       .then((data) => {
-        return this.#getUserInfo(data,id);
+        return this.#getUsers(data, isAll, fields);
       })
       .catch(console.error);
   }
 
-  static save(userInfo) {
-    // const users = this.#users;
+  static getUserInfo(id) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
+  }
+
+  static async save(userInfo) {
+    const users = await this.getUsers(true);
+
+    if (users.id.includes(userInfo.id)) {
+      throw "이미 존재하는 아이디입니다";
+    }
     users.id.push(userInfo.id);
-    users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
+    users.name.push(userInfo.name);
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
     return { success: true };
   }
 }
