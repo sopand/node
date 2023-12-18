c8c8ca67906a943f322fb6360acd73e41ff106e7
sopand <pows1031@naver.com>
2023-12-18 13:19:26 +0900

    데이터베이스 파일 저장

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,47 +1,44 @@
 "use strict";
 
+const fs = require("fs");
 
+class UserStorage {
+  // ...변수명으로 파라미터를 받아오면 해당 파라미터에 갯수 제한을 두는게아닌 배열로 받아와 여러 파라미터를 받을 수 있음
+  static getUsers(...fields) {
+    // 자바의 Getter원리인듯 함 바로 가져오는게 아닌 Getter를 거쳐서 데이터를 가져오는 것
+    // const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
 
-class UserStorage{
-        // # 을 변수에 붙히면 public 변수 => private 변수로 변경시켜준다 은닉화
-   static  #users={  //해당 변수를 require한 상태로 바로 불러오기 위해서는 static을 선언하여 정적변수로 만들어 줘야 한다. 일반 변수는 바로사용 불가 x new 초기화해줘야 불러올 수 있음
-        id:["pows1011","pows1021","pows1031"],
-        psword:["1234","2345","3456"],
-        name:["aaa","bbb","ccc"],
-    };
-                                // ...변수명으로 파라미터를 받아오면 해당 파라미터에 갯수 제한을 두는게아닌 배열로 받아와 여러 파라미터를 받을 수 있음
-    static getUsers(...fields){   // 자바의 Getter원리인듯 함 바로 가져오는게 아닌 Getter를 거쳐서 데이터를 가져오는 것
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers,field)=>{
-        if(users.hasOwnProperty(field)){
-            newUsers[field]=users[field];
-        }
-        return newUsers;
-        },{});
- 
-        return newUsers;
-    }
+    return newUsers;
+  }
 
-    static getUserInfo (id){
-        const users=this.#users;
-        const idx=users.id.indexOf(id);
-        const usersKeys=Object.keys(users);
-        const userInfo =usersKeys.reduce((newUser,info)=>{
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-        return userInfo;
-    }
+  static getUserInfo(id) {
 
+    fs.readFile("./src/databases/users.json", (err, data) => {
+      if (err) throw err;
+      const users = JSON.parse(data);
+      const idx = users.id.indexOf(id);
+      const usersKeys = Object.keys(users);
+      const userInfo = usersKeys.reduce((newUser, info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
+      }, {});
+      return userInfo;
+    });
+  }
 
-    static save(userInfo){
-        const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psword.push(userInfo.psword);
-        return {success:true};
-    }
-    
+  static save(userInfo) {
+    // const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.psword.push(userInfo.psword);
+    return { success: true };
+  }
 }
 
 module.exports = UserStorage;
