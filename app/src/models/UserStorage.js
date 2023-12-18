6a5b2173dc14c85d701860e6bb1640c238acbd8f
sopand <pows1031@naver.com>
2023-12-18 22:40:03 +0900

    morgan Log 설정

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -16,9 +16,8 @@ class UserStorage {
   }
 
   static async save(userInfo) {
-    
     return new Promise((resolve,reject)=>{
-      const query="INSERT INTO users (id,name,psword)VALUES(?,?,?)";
+      const query="INSERT INTO usersdas (id,name,psword)VALUES(?,?,?)";
         db.query(query,
           [userInfo.id,userInfo.name,userInfo.psword],
           (err)=>{
